931587bd5f2a908419e65185e12e9a2e6809bfe5
AJAmit17 <amitjagadeesh2004@gmail.com>
2024-06-13 19:54:27 +0530

    hourly styles update

--- weather/forms.py
+++ weather/forms.py
@@ -2,4 +2,7 @@
 from django import forms
 
 class CityForm(forms.Form):
-    city = forms.CharField(label='Enter city name', max_length=100)
+    city = forms.CharField(label='Enter city name', max_length=100, widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'e.g., Bengaluru'
+    }))
